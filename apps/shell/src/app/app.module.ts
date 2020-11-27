902b900590b4c44f19f06e790b70f5c51f026bd4
Guilherme Santos <guilherme.santos@sap.com>
2020-11-27 18:28:52 +0000

    Dashboard component created

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, DashboardComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ apps/shell/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+export const APP_ROUTES: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+    pathMatch: 'full',
+  },
+];
