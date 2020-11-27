6a2b2f77d31c4a283e890499a1789e0554327e64
Guilherme Santos <guilherme.santos@sap.com>
2020-11-27 18:55:52 +0000

    added module federation to shell

---
+++ apps/shell/webpack.config.js
@@ -0,0 +1,43 @@
+import ModuleFederationPlugin from 'webpack/lib/container/ModuleFederationPlugin';
+import mf from '@angular-architects/module-federation/webpack';
+import path from 'path';
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(path.join(__dirname, '../../tsconfig.base.json'), [
+  /* mapped paths to share */
+]);
+
+module.exports = {
+  output: {
+    uniqueName: 'shell',
+  },
+  optimization: {
+    // Only needed to bypass a temporary bug
+    runtimeChunk: false,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      // For remotes (please adjust)
+      // name: 'shell',
+      // filename: 'remoteEntry.js',
+      // exposes: {
+      //   './Component': './apps/shell/src/app/app.component.ts',
+      // },
+
+      // For hosts (please adjust)
+      // remotes: {
+      //     "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
+
+      // },
+
+      shared: {
+        '@angular/core': { singleton: true, strictVersion: true },
+        '@angular/common': { singleton: true, strictVersion: true },
+        '@angular/router': { singleton: true, strictVersion: true },
+
+        ...sharedMappings.getDescriptors(),
+      },
+    }),
+    sharedMappings.getPlugin(),
+  ],
+};
