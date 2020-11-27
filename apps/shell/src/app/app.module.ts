d9f6a88b326ee9506a1950dba4e91dd5236d5762
Guilherme Santos <guilherme.santos@sap.com>
2020-11-27 18:33:35 +0000

    shell routing added

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { APP_ROUTES } from './app.routes';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [AppComponent, DashboardComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, RouterModule.forRoot(APP_ROUTES)],
   providers: [],
   bootstrap: [AppComponent],
 })
