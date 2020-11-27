b85f48272695f6433a83acfd2561d5992535e8d9
Guilherme Santos <guilherme.santos@sap.com>
2020-11-27 22:39:06 +0000

    header component & button changes

--- apps/shell/src/app/app.module.ts
+++ apps/shell/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { ButtonModule } from '@lightning-components';
 import { AppComponent } from './app.component';
 import { APP_ROUTES } from './app.routes';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
+  declarations: [AppComponent, DashboardComponent, HeaderComponent],
   imports: [ButtonModule, BrowserModule, RouterModule.forRoot(APP_ROUTES)],
   providers: [],
   bootstrap: [AppComponent],
