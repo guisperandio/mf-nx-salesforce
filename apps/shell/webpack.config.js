1c7e48136c3ede13b9869f484abe0199c7989b20
Guilherme Santos <guilherme.santos@sap.com>
2020-11-30 19:25:37 +0000

    fix packages

--- apps/shell/webpack.config.js
+++ apps/shell/webpack.config.js
@@ -1,35 +1,35 @@
-// /* eslint-disable no-undef */
-// /* eslint-disable @typescript-eslint/no-var-requires */
-// const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
-// module.exports = {
-//   output: {
-//     uniqueName: 'shell',
-//   },
-//   optimization: {
-//     // Only needed to bypass a temporary bug
-//     runtimeChunk: false,
-//   },
-//   plugins: [
-//     new ModuleFederationPlugin({
-//       // For remotes (please adjust)
-//       // name: "shell",
-//       // filename: "remoteEntry.js",
-//       // exposes: {
-//       //     './Component': './apps/shell/src/app/app.component.ts',
-//       // },
+module.exports = {
+  output: {
+    uniqueName: 'shell',
+  },
+  optimization: {
+    // Only needed to bypass a temporary bug
+    runtimeChunk: false,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      // For remotes (please adjust)
+      // name: "shell",
+      // filename: "remoteEntry.js",
+      // exposes: {
+      //     './Component': './apps/shell/src/app/app.component.ts',
+      // },
 
-//       // For hosts (please adjust)
-//       // remotes: {
-//       //     "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
+      // For hosts (please adjust)
+      // remotes: {
+      //     "mfe1": "mfe1@http://localhost:3000/remoteEntry.js",
 
-//       // },
+      // },
 
-//       shared: {
-//         '@angular/core': { singleton: true, strictVersion: true },
-//         '@angular/common': { singleton: true, strictVersion: true },
-//         '@angular/router': { singleton: true, strictVersion: true },
-//       },
-//     }),
-//   ],
-// };
+      shared: {
+        '@angular/core': { singleton: true, strictVersion: true },
+        '@angular/common': { singleton: true, strictVersion: true },
+        '@angular/router': { singleton: true, strictVersion: true },
+      },
+    }),
+  ],
+};
